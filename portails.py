3fc269673945869f2c117fbd0979275e5bc4c880
Caroline Lena <caroline.becker@wikimedia.fr>
2018-05-17 08:41:25 +0200

    Test

--- portails.py
+++ portails.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import mwclient
+import json
 from mwclient import page
 
 portail = re.compile(u"{{Portail\|[\w|\|| |'|à|ç|é|è|ê|ë|ï|î|û|ù]+}}")
@@ -12,16 +13,17 @@ def portails(string):
     if search is not None:
         return separation.split(search.group())
     else:
-        print string
+        print "No portal found"
         return[]
 
 def portailsDe(pages):
     result = {}
     for title in pages:
-        result[title] = {}
-        print title
-        page = site.Pages[title.encode().decode()]
-        result[title]["Portails"] = [i for i in portails(page.text()) if len(i) > 0]
+        if len(title) > 0:
+            result[title] = {}
+            print title
+            page = site.Pages[title.decode('utf-8')]
+            result[title]["Portails"] = [i for i in portails(page.text()) if len(i) > 0]
     with open("dump.json", "w") as file:
         data = json.dumps(result, indent=2)
         file.write(data)
